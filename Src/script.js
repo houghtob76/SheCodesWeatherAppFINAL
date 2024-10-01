3c88ee3ccb6d3bb9ab64294e0e5e9b35d2c7ff1b
houghtob76 <houghtob@mail.gvsu.edu>
2024-09-30 21:49:16 -0400

    Javascriptyupdates
    Humidity, wind, icon, css fixes

--- Src/script.js
+++ Src/script.js
@@ -2,8 +2,18 @@ function displayTemperature(response) {
     let temperatureElement = document.querySelector("#current-temperature");
     let temperature = Math.round(response.data.temperature.current);
     let cityElement = document.querySelector("#current-city");
+    let mood = document.querySelector("#current-mood");
+    let humidity = document.querySelector("#current-humidity");
+    let wind = document.querySelector("#current-wind");
+    let icon = document.querySelector("#icon-image");
+    
+    
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = temperature;
+    mood.innerHTML = ` ${response.data.condition.description} `;
+    humidity.innerHTML = ` ${response.data.temperature.humidity}%,`;
+    wind.innerHTML = ` ${response.data.wind.speed}`;
+    icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon" />`;
 }
 
 function search(event) {
@@ -12,8 +22,10 @@ function search(event) {
     let city = searchInputElement.value;
     let apiKey = "234aebbda877t7f1bcab0617d23f3o88";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  
+
+
     axios.get(apiUrl).then(displayTemperature);
+
 
 }
 
@@ -42,6 +54,8 @@ function formatDate(date) {
 
     let formattedDay = days[day];
     return `${formattedDay} ${hours}:${minutes}`;
+
+
 }
 
 let searchForm = document.querySelector("#search-form");
